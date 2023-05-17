49c0961e2b9bb74480dce2ac1160183aa9bde84e
junguk03 <97594871+junguk03@users.noreply.github.com>
2023-05-17 13:50:16 +0900

    Add files via upload

---
+++ 4_1_2_if_app.py
@@ -0,0 +1,6 @@
+input_id = input("id: ")
+id = 'junguk'
+if input_id == id:
+    print("Hello")
+else:
+    print("꺼져")
---
+++ 4_3_2_if_app.py
@@ -0,0 +1,9 @@
+input_id = input("id: ")
+name1 = 'junguk'
+name2 = 'sam'
+if input_id == name1:
+    print("Welcome")
+elif input_id == name2:
+    print("Welcome")
+else:
+    print("꺼져")     
